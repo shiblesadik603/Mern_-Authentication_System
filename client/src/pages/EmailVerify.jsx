ce924cd89b8b918de12e0c7385c75f6b30351c09
shiblesadik603 <smsadik82@gmail.com>
2025-11-05 19:56:31 +0600

    Added

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -18,6 +18,16 @@ const EmailVerify = () => {
     }
   }
 
+  const handlePaste = (e) => {
+    const paste = e.clipboardData.getData('text')
+    const pasteArray = paste.split('');
+    pasteArray.forEach((char, index)=>{
+      if(inputRefs.current[index]){
+        inputRefs.current[index].value = char;
+      }
+    })
+  }
+
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
@@ -27,7 +37,7 @@ const EmailVerify = () => {
         </h1>
         <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your email id.</p>
 
-        <div className='flex justify-between mb-8'>
+        <div className='flex justify-between mb-8' onPaste={handlePaste}>
           {Array(6).fill(0).map((_, index)=>(
             <input type="text" maxLength='1' key={index} required 
             className='w-12 h-12 bg-[#333A5C] text-white text-center text-xl rounded-md'
